e15cc94874e66be7b8f61afe7df0900021e8528b
ilbertt <luca.bertelli@dfinity.org>
2025-09-26 12:16:19 +0200

    chore: prepare branch

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { tanstackViteConfig } from '@tanstack/config/vite';
-
-export default tanstackViteConfig({
-  entry: ['./src/index.ts', './src/client/index.ts'],
-  srcDir: './src',
-  outDir: './dist',
-  tsconfigPath: './tsconfig.lib.json',
-  cjs: false,
-});
